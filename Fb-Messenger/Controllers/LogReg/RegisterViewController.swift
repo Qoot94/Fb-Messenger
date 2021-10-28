9ffb1bda35f289c9c85814db8a771633a6806f2a
Qoot Alkulaib <qootalkulaib@hotmail.com>
2021-10-28 14:52:21 +0300

    section 3:upload photo

--- Fb-Messenger/Controllers/LogReg/RegisterViewController.swift
+++ Fb-Messenger/Controllers/LogReg/RegisterViewController.swift
@@ -17,17 +17,15 @@ class RegisterViewController: UIViewController {
         profileIMG.clipsToBounds=true
         profileIMG.layer.borderColor=UIColor.black.cgColor
         // Do any additional setup after loading the view.
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.profilePicPicked(tapGestureRecognizer:)))
+           //imageView.isUserInteractionEnabled = true
+        profileIMG.addGestureRecognizer(tapGestureRecognizer)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func profilePicPicked(tapGestureRecognizer: UITapGestureRecognizer){
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        self.presentPhotoActionSheet()
     }
-    */
 
 }
