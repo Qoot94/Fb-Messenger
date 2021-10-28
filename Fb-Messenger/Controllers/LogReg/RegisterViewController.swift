1510c46e124d11f5f4fd02752f6de0ba6e741b97
Qoot Alkulaib <qootalkulaib@hotmail.com>
2021-10-28 14:28:52 +0300

    section3: upload photo

--- Fb-Messenger/Controllers/LogReg/RegisterViewController.swift
+++ Fb-Messenger/Controllers/LogReg/RegisterViewController.swift
@@ -16,18 +16,16 @@ class RegisterViewController: UIViewController {
         profileIMG.layer.cornerRadius = profileIMG.bounds.width / 2
         profileIMG.clipsToBounds=true
         profileIMG.layer.borderColor=UIColor.black.cgColor
+       
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.pickProfilePhoto(tapGestureRecognizer:)))
+        profileIMG.isUserInteractionEnabled = true
+        profileIMG.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func pickProfilePhoto(tapGestureRecognizer: UITapGestureRecognizer){
+        let ImagePressed = tapGestureRecognizer.view as! UIImageView
+        self.presentPhotoActionSheet()
+        print("tapppp")
     }
-    */
 
 }
--- Fb-Messenger/Resources/Extensions.swift
+++ Fb-Messenger/Resources/Extensions.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import UIKit
+
+//section 3:  image picker controller
+extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // get results of user taking picture or selecting from camera roll
+    func presentPhotoActionSheet(){
+        let actionSheet = UIAlertController(title: "Profile Picture", message: "How would you like to select a picture?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { [weak self] _ in
+            self?.presentCamera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
+        
+        present(actionSheet, animated: true)
+    }
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // take a photo or select a photo
+        
+        // action sheet - take photo or choose photo
+        picker.dismiss(animated: true, completion: nil)
+        print(info)
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        
+        self.profileIMG.image = selectedImage
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
